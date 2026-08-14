2317753a91fb49382a0e8c9fb502dddd693ac75c
yotamc7494 <yotamcohem7494@gmail.com>
2026-08-14 15:23:01 +0300

    new video type

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ st.header("סרטוני מנייה")
 # יצירת עמודות נפרדות עבור סקשן זה
 col1, col2 = st.columns(2)
 
-if "token" in st.query_params:
-  if st.query_params["token"] == st.secrets.get("WEBHOOK_TOKEN"):
+if "WEBHOOK_TOKEN" in st.query_params:
+  if st.query_params["WEBHOOK_TOKEN"] == st.secrets.get("WEBHOOK_TOKEN"):
     st.info("⚡ הופעל מרחוק דרך Webhook!")
 
     # הרצת היצירה
